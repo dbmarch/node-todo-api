768860a57a6f2c9fe2ee24ac54d7b70db78b4355
dbmarch <dbmarch@gmail.com>
2018-08-04 22:34:55 -0500

    setup for heroku

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ var { Todo } = require("./models/todo");
 var { User } = require("./models/user");
 const { ObjectId } = require("mongodb");
 
+const port = process.env.port || 3001;
+
 var app = express();
 app.use(bodyParser.json());
 
@@ -58,7 +60,7 @@ app.get("/todos/:id", (req, res) => {
     });
 });
 
-app.listen(3001, () => {
+app.listen(port, () => {
   console.log("Started on port 3001");
 });
 
