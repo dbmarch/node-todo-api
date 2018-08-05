9d500a1b7e4b67603e2b7bdbaa753db2ef31ee50
dbmarch <dbmarch@gmail.com>
2018-08-04 20:59:18 -0500

    Test Get /todos

--- server/server.js
+++ server/server.js
@@ -1,39 +1,45 @@
-var express = require ('express');
-var bodyParser = require ('body-parser');
+var express = require("express");
+var bodyParser = require("body-parser");
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require ('./models/todo');
-var {User} = require ('./models/user');
+var { mongoose } = require("./db/mongoose");
+var { Todo } = require("./models/todo");
+var { User } = require("./models/user");
 
 var app = express();
-app.use (bodyParser.json());
+app.use(bodyParser.json());
 
-app.post ('/todos' , (req, res) => {
-//    console.log ('POST /todos');
-    console.log (req.body);
+app.post("/todos", (req, res) => {
+  //    console.log ('POST /todos');
+  //   console.log(req.body);
 
-    var todo = new Todo ({
-        text: req.body.text
-    });
+  var todo = new Todo({
+    text: req.body.text
+  });
 
-    todo.save().then((doc)=> {
-        res.send(doc);
-    }, (e) => {
-        res.status(400).send(e);
-    });
+  todo.save().then(
+    doc => {
+      res.send(doc);
+    },
+    e => {
+      res.status(400).send(e);
+    }
+  );
 });
 
-app.get ('/todos', (req,res) => {
-    Todo.find().then((todos) => {
-        console.log ('Returning ', todos)
-        res.send({todos})
-    }, (e) => {
-        res.status(400).send(e);
-    });
+app.get("/todos", (req, res) => {
+  Todo.find().then(
+    todos => {
+      // console.log ('Returning ', todos)
+      res.send({ todos });
+    },
+    e => {
+      res.status(400).send(e);
+    }
+  );
 });
 
-app.listen (3001, () => {
-    console.log ('Started on port 3001');
+app.listen(3001, () => {
+  console.log("Started on port 3001");
 });
 
-module.exports = {app};
+module.exports = { app };
