1c9f4ed4ef4928a01df068963e7bbeafeae406ed
dbmarch <dbmarch@gmail.com>
2018-08-04 22:06:40 -0500

    Get Todos/:id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require("body-parser");
 var { mongoose } = require("./db/mongoose");
 var { Todo } = require("./models/todo");
 var { User } = require("./models/user");
+const { ObjectId } = require("mongodb");
 
 var app = express();
 app.use(bodyParser.json());
@@ -36,6 +37,25 @@ app.get("/todos", (req, res) => {
       res.status(400).send(e);
     }
   );
+});
+
+app.get("/todos/:id", (req, res) => {
+  var id = req.params.id;
+  if (!ObjectId.isValid(id)) {
+    return res.status(404).send("ID not valid");
+  }
+
+  Todo.findById(id)
+    .then(todo => {
+      if (todo) {
+        res.send({ todo });
+      } else {
+        res.status(404).send("not found");
+      }
+    })
+    .catch(e => {
+      res.status(400).send();
+    });
 });
 
 app.listen(3001, () => {
